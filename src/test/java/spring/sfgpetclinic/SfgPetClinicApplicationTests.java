7cc9f76b2d4bacc2acd6336dea958e97154f7057
hazem-atya <hazematya1@gmail.com>
2020-12-02 17:42:29 +0100

    First commit

---
+++ src/test/java/spring/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
